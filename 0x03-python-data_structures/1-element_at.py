96266ff0161ac609440133e46096f9f4c280e129
Sado1111 <sadohannahfabohns@gmail>
2024-01-24 04:25:50 -0800

    removed the use of None strings ams changed comparisom operator as well as values

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lens = len(my_list)
-    if (idx < -(lens) | idx > (lens - 1)):
-        return (None)
-    else:
+    if (idx > -(lens + 1) | idx < lens):
         return (my_list[idx])
