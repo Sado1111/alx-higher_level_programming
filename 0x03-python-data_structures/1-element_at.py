6146f712ae019b6946469d17c3e55cf84be7c784
Sado1111 <sadohannahfabohns@gmail>
2024-01-24 04:22:19 -0800

    changedthe code to make provision for when index is negative, representing index count from the back

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (idx >= 0 | idx < (len(my_list) - 1)):
+    lens = len(my_list)
+    if (idx < -(lens) | idx > (lens - 1)):
+        return (None)
+    else:
         return (my_list[idx])
