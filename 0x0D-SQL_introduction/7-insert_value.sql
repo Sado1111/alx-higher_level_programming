a7ec9457a11bd99bd7fa3c80b0d912cdf712724d
Sado1111 <sadohannahfabohns@gmail.com>
2024-05-21 18:18:08 +0100

    put string in single quote

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- inserts new row and its values to a table, first_table in a database - it inserts values
-INSERT INTO first_table(id, name) VALUES (89, Best School);
+-- inserts a new row and its values to a table, first_table in a database - it inserts values
+INSERT INTO first_table(id, name) VALUES (89, 'Best School');
