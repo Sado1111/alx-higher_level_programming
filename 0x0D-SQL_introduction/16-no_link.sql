1061a720dd01cdd266d1e30a55cd8e681f1b2325
Sado1111 <sadohannahfabohns@gmail.com>
2024-05-21 19:28:57 +0100

    had to put field at which the selected list is ordered

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists records of second_table where name is not null, selected by score,name in descending order
-SELECT score, name FROM second_table WHERE name IS NOT null ORDER BY DESC;
+SELECT score, name FROM second_table WHERE name IS NOT null ORDER BY score DESC;
