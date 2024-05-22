c79aa6ec5dc75d8086614252321024e3c85d989f
Sado1111 <sadohannahfabohns@gmail.com>
2024-05-22 02:08:04 +0100

    did not convert  the database just a table to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
 -- Convert the entire database to utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+USE `hbtn_0c_0`
 -- Convert the table first_table to utf8mb4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
