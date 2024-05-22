c8e95d5a7f27169dc8454f3bc9a2ba168acda0c6
Sado1111 <sadohannahfabohns@gmail.com>
2024-05-22 17:06:01 +0100

    added server that users are using @localhost

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all priviledges granted i.e. show all priviledges given to user_0d_1 and user_0d_2
-SHOW GRANT FOR user_0d_1
-SHOW GRANT FOR user_0d_2
+SHOW GRANT FOR user_0d_1@localhost
+SHOW GRANT FOR user_0d_2@localhost
