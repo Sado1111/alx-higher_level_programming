66088bea21d072a664f756895d571aab595d93a3
Sado1111 <sadohannahfabohns@gmail.com>
2024-05-22 18:39:20 +0100

    placed s at the back of state in states table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,5 +4,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(id)
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
