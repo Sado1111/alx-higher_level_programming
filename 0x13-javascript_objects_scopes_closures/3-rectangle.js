f2ba3c637011bdb33f5f5fdfb7a5476e8e271dbb
Sado1111 <sadohannahfabohns@gmail.com>
2024-06-20 19:29:31 +0100

    created sevearl developed class Rectangle definitions

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.width; i++) console.log('x'.repeat(this.height));
+  }
+}
+module.exports = Rectangle;
