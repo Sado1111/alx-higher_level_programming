4dc3e1f61e9e630c04a0394dc5b685f8d6746df1
Sado1111 <sadohannahfabohns@gmail.com>
2024-06-21 05:25:08 +0100

    removed quote from variable

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@ const Parent = require('./5-square.js');
 class Square extends Parent {
   charPrint (c) {
     if (c === undefined) (c = 'X');
-    for (let i = 0; i < this.height; i++) console.log('c'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
   }
 }
 module.exports = Square;
