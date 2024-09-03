fa5640b2472023a21a9e0ae6d482d64a888bfe5d
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 10:41:42 +0100

    changed print function to be string format

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 """ program prints numbers  0 to 98 in decimal and in hexadecimal """
+
 for i in range(99):
-    print(f'{i:d} = 0x{i:x}')
+    print('{:d} = 0x{:x}'.format(i, i))
