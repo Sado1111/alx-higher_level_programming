97f6f8a9d24799194f04c3cf2e95743ea763aa1b
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 12:25:52 +0100

    created a function that prints and returns last dits of number passed into it

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Function that prints and returns the last digit of a number.
+You are not allowed to import any module
+You don’t need to understand __import__
+"""
+
+
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print('{}'.format(last_digit), end='')
+    return (last_digit)
