ed2e3c3227483e3fa56fa3ea4831122bd6d88a7f
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 16:16:26 +0100

    only respond if the my_list is not NULL

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,8 @@ a specific position without modifying the original list
 
 
 def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
-    if idx > -1 annd idx < len(my_list):
-        new_list[idx] = element
-    return new_list
+    if my_list:
+        new_list = my_list.copy()
+        if idx > -1 annd idx < len(my_list):
+            new_list[idx] = element
+        return new_list
