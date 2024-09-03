ad03e2f90198bcc5b64ee93123e53ecc69cef79c
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 16:12:19 +0100

    created a copy of original list and returned the copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that replaces an element in a list at
+a specific position without modifying the original list
+"""
+
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx > -1 annd idx < len(my_list):
+        new_list[idx] = element
+    return new_list
