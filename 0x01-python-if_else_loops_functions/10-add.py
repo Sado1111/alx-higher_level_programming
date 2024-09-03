f643ccce6ee304b27b2d47b5458c3b195f38b951
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 12:34:35 +0100

    function that returns the sum of two numbers passed to it

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Write a function that adds two integers and returns the result.
+Prototype: def add(a, b):
+Returns the value of a + b
+"""
+
+
+def add(a, b):
+    return (a + b)
