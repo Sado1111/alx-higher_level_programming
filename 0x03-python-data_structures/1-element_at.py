90eb5b34659dbf77cf8424ae763bc0a906e8a711
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 15:54:38 +0100

    used the right logicaloperator - not OR but AND

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lens = len(my_list)
-    if (idx > -1 | idx < lens):
+    if (idx > -1 and idx < lens):
         return (my_list[idx])
     return None
