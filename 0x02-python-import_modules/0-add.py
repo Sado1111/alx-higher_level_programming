6299c37dc11abe68bebfd2d6e753d41bd2c53f42
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 15:23:25 +0100

    changed identifier of variable for the sums of a and b

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,12 +2,11 @@
 """
 program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
 """
-
+from add_0 import add
 
 if __name__ == "__main__":
-    from add_0 import add
 
     a = 1
     b = 2
-    sum = add(a, b)
-    print("{} + {} = {:d}".format(a, b, sum))
+    sums = int(add(a, b))
+    print("{} + {} = {:d}".format(a, b, sums))
