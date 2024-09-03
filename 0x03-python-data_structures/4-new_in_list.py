305c02c32df205595f99a50e9ddbdd8535036846
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 16:18:03 +0100

    decided not to use copy()

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,7 +7,7 @@ a specific position without modifying the original list
 
 def new_in_list(my_list, idx, element):
     if my_list:
-        new_list = my_list.copy()
+        new_list = my_list[:]
         if idx > -1 annd idx < len(my_list):
             new_list[idx] = element
         return new_list
