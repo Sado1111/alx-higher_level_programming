5cdc68b7fba60d29bfb1f234622f25c1ca03bbb4
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 10:31:26 +0100

    used the right logical operator

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """ program prints the ASCII alphabet (except q and e) in lowercase."""
 for i in range(97, 123):
-    if i != ord('q') and i != ord('e'):
+    if i != ord('q') or i != ord('e'):
         print('{}'.format(chr(i)i), end='')
