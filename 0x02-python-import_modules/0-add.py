1b18cd81a1004503693751f2f14526c3a9a73980
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 15:41:04 +0100

    chnaged import statemnt location

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,9 +4,9 @@ program that imports the function def add(a, b):
     from the file add and prints the result of the addition 1 + 2 = 3
 """
 
+from 0-import_add import add
 
 if __name__ == "__main__":
-    from 0-import_add import add
 
     a = 1
     b = 2
