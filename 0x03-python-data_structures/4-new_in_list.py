fb0c9a3c70dca9f4ce4b6487de10d4615e8ed40f
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 16:25:23 +0100

    corrected a typo

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -9,4 +9,4 @@ def new_in_list(my_list, idx, element):
     if my_list and idx > -1 and idx < len(my_list):
         new_list[idx] = element
         return new_list
-    return (mu_list)
+    return (my_list)
