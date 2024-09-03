854489b75ffb61b97649ab900b8ea68f6f1711cb
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 15:38:01 +0100

    tried changing location of import statement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,10 +5,8 @@ program that imports the function def add(a, b):
 """
 
 
-from 0-import_add import add
-
 if __name__ == "__main__":
-    """    from add_0 import add   """
+    from 0-import_add import add
 
     a = 1
     b = 2
