5862abc86ed5972e6e4676c13e4dbe999b58b906
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 15:13:55 +0100

    removed .y extension when importing add function from the add module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
+"""
+
+
 if __name__ == "__main__":
-    from add_0.py import add
+    from add_0 import add
 
     a = 1
     b = 2
