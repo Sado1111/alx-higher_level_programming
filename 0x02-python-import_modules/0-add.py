c7befc3f53f63b41f23566d9e90dfbb6e42d365d
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-03 15:25:27 +0100

    place import statement after ensuring that this code is executed if only executed not when file is imported

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,10 @@
 """
 program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
 """
-from add_0 import add
+
 
 if __name__ == "__main__":
+    from add_0 import add
 
     a = 1
     b = 2
