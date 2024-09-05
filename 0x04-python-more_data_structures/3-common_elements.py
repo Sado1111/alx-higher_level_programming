e61b139d4eddbb0075c48fd70e67d8d5f51ca05d
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-05 19:25:55 +0100

    gets the intersection of two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Function that returns a set of common elements in two sets.
+
+    Prototype: def common_elements(set_1, set_2):
+    You are not allowed to import any module
+"""
+
+
+def common_elements(set_1, set_2):
+    return set_1 & set_2
