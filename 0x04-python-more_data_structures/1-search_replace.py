e97f8f57bf475bbe6d56a203a865ff5fa71b0a17
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-05 17:26:09 +0100

    function returns a list that replaces anyplaces eelaces anyy occurence of search variable value with replace variable value

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that replaces all occurrences of an element
+ by another in a new list.
+    Prototype: def search_replace(my_list, search, replace):
+    my_list is the initial list
+    search is the element to replace in the list
+    replace is the new element
+    You are not allowed to import any module
+
+"""
+
+
+def search_replace(my_list, search, replace):
+    return [list(map(lambda x: replace if x == search else x, my_list))]
