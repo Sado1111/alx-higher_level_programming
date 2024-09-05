3524efc9dce7b431e53bff4d6e617ea89096a850
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-05 19:50:24 +0100

    paced items() method in the right location

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -13,6 +13,6 @@ function that prints a dictionary by ordered keys.
 """
 
 
-def print_sorted_dictionary(a_dictionary.items()):
-    for key, value in sorted(a_dictionary):
+def print_sorted_dictionary(a_dictionary):
+    for key, value in sorted(a_dictionary.items()):
         print('{}: {}'.format(key, value))
