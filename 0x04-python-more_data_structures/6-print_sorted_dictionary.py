26d5eda88ae75a9d1401c590c8b9f5001c13df51
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-05 19:48:45 +0100

    added .items() method to the dictionary iterated

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -13,6 +13,6 @@ function that prints a dictionary by ordered keys.
 """
 
 
-def print_sorted_dictionary(a_dictionary):
+def print_sorted_dictionary(a_dictionary.items()):
     for key, value in sorted(a_dictionary):
         print('{}: {}'.format(key, value))
