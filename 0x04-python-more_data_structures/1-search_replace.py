7c7381aeb2d5acb405445948c19d62a07c9ac350
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-05 17:29:38 +0100

    removed square bracket enclosure

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -12,4 +12,4 @@ function that replaces all occurrences of an element
 
 
 def search_replace(my_list, search, replace):
-    return [list(map(lambda x: replace if x == search else x, my_list))]
+    return (list(map(lambda x: replace if x == search else x, my_list)))
