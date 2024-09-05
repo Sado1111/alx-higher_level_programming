43789696fba715f757608a6355bb795f596fa422
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-05 11:34:34 +0100

    return original list if conditions and not met else return modified copy of the list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,6 +7,7 @@ a specific position without modifying the original list
 
 def new_in_list(my_list, idx, element):
     if my_list and idx > -1 and idx < len(my_list):
+        new_list = my_list.copy()
         new_list[idx] = element
         return new_list
     return (my_list)
